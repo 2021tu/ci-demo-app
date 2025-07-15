642f683397c8a7c5d3a1ec150e128e3aab2323ea
2021tu <2021wc86596@wilp.bits-pilani.ac.in>
2025-07-15 21:38:00 +0530

    Fixed App.java to test Poll SCM

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -6,9 +6,11 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello from Jenkins CI!");
+        System.out.println("Modified code to test Poll SCM trigger!");
 
     }
 
 }
+
+
 
